7313a810c8168d460ebb31ffeff8a579fae0ed04
Bahman Azimi <bahmanazimi1987@gmail.com>
2023-10-27 09:52:17 +0300

    add some comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-    
+    // Create a space between every three numbers in the output
     const changResult = () => {
         const resultTxt = document.querySelector('.result-txt');
     
@@ -13,17 +13,16 @@
         }
     }
     
-  
-    
-    
-    
+//   Empty the input and output
     const clear_txt = () => {
         document.querySelector('.inputs-txt').innerText = '';
         document.querySelector('.result-txt').innerText = '';     
     }
-
+// Making negative or positive input numbers
     const pos_neg = () => { 
-        if (document.querySelector('.inputs-txt').innerText.length < 30) {
+       
+        if (document.querySelector('.inputs-txt').innerText.length < 30)  // Creating an input limit of up to 30 characters
+         {
             const input = document.querySelector('.inputs-txt');
             input.innerText = input.innerText.startsWith('-') ?
              input.innerText.substring(1) : '-' + input.innerText;   
@@ -32,10 +31,11 @@
         
     }
 
-
+// input numbers
     const addNumber = (num) => {
         if (document.querySelector('.inputs-txt').innerText.length < 30) {
-            if (document.querySelector('.result-txt').innerText) {
+            if (document.querySelector('.result-txt').innerText) // Clearing previous data before a new entry is entered
+             {
             document.querySelector('.inputs-txt').innerText = '';
             document.querySelector('.result-txt').innerText = '';
             const input = document.querySelector('.inputs-txt');
@@ -45,33 +45,31 @@
             input.innerText += num;
         }
         }
-
-       
        
     }
-
+// input functions
     const addSymbol = (symbol) => {
         if (document.querySelector('.inputs-txt').innerText.length < 30) {
             const input = document.querySelector('.inputs-txt');
             if (input.innerText === '' &&
              (symbol === '*' || symbol === '/' || symbol === '+' 
-             || symbol === '-' || symbol === '%' || symbol === '.')) {
+             || symbol === '-' || symbol === '%' || symbol === '.')) // If there is no number before the entered symbol
+              {
                 input.innerText = '0' + symbol;
             } else {
                 input.innerText += symbol;
             }
         }
-
-         
     }
-
+// calculation
     const equals = () => {
-        
-        const inputText = document.querySelector('.inputs-txt').innerText;
+       
+            const inputText = document.querySelector('.inputs-txt').innerText;
             const result = eval(inputText);
             if (document.querySelector('.result-txt').innerText &&
-             document.querySelector('.result-txt').innerText !== 'Error' ) {
-                document.querySelector('.inputs-txt').innerText = result;
+             document.querySelector('.result-txt').innerText !== 'Error' )  // If there was a number in the output 
+             {
+                document.querySelector('.inputs-txt').innerText = result;  // Transfer the result to the input field for further operation
                 document.querySelector('.result-txt').innerText = '';
             } else{  
                    if (isNaN(result) || !isFinite(result)) {
